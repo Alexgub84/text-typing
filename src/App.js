6c4d1ce116fff8dc4c8be913dcb85aeaf295a301
Alex Guberman <alex.gub@gmail.com>
2021-06-02 13:42:10 +0300

    first

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import {TextTyping} from './cmp/TextTyping'
+function App() {
+  return (
+    <div className="App">
+      <TextTyping text={"Gloat"} seconds={1000}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/cmp/TextTyping.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect, useRef } from "react";
+
+export function TextTyping({ text, seconds }) {
+  const [currIndex, setCurrIndex] = useState(0);
+  const [textToDisplay, setTextToDisplay] = useState("");
+  const interval = useRef();
+
+  useEffect(() => {
+    reStartTimerAndState()
+    return () => {
+      clearInterval(interval.current);
+    };
+  }, []);
+
+  useEffect(() => {
+    
+    if (currIndex === text.length) {
+        
+        reStartTimerAndState()
+    }else{
+        setTextToDisplay((prevText) => prevText + text.charAt(currIndex));
+    }
+  }, [currIndex]);
+
+  function nextCount() {
+    setCurrIndex((prevIndex) => prevIndex + 1)
+  }
+  function reStartTimerAndState(){
+    setCurrIndex(0)
+    setTextToDisplay("")
+    clearInterval(interval.current);
+    interval.current = setInterval(nextCount, seconds);
+  }
+
+  return (
+    <div>
+      {textToDisplay}
+    </div>
+  );
+}
